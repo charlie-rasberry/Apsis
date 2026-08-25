cf59c18273a71b2814fb03e1c42b25aa873fa8a0
charlie-rasberry <charlie.rasberry@outlook.com>
2026-08-25 16:47:04 +0100

    nothing, started ecef on Coordinates.cs

--- samples/Apsis.Coordinates.Demo/Program.cs
+++ samples/Apsis.Coordinates.Demo/Program.cs
@@ -6,60 +6,6 @@ using Apsis.Time;
 // currently doesn't account for days
 while (true)
 {
-    Console.WriteLine("INTERACTIVE MODE ? [Y]/n");
-    string? input = Console.ReadLine()?.ToLower();
-    if (input is "y" or "")
-    {
-        Console.WriteLine("Enter a date, year / month / day / hour");
-        Console.WriteLine("year: ");
-        input = Console.ReadLine();
-        if (long.TryParse(input, out long y))
-        {
-            Console.WriteLine("Ok");
-        }
-        else
-        {
-            Console.WriteLine("Error");
-        }
-
-        Console.WriteLine("month: ");
-        input = Console.ReadLine();
-        if (long.TryParse(input, out long m))
-        {
-            Console.WriteLine("Ok");
-        }
-        else
-        {
-            Console.WriteLine("Error");
-        }
-
-        Console.WriteLine("day: ");
-        input = Console.ReadLine();
-        if (long.TryParse(input, out long d))
-        {
-            Console.WriteLine("Ok");
-        }
-        else
-        {
-            Console.WriteLine("Error");
-        }
-
-        Console.WriteLine("ut1: ");
-        input = Console.ReadLine();
-        if (double.TryParse(input, out double h))
-        {
-            Console.WriteLine("Ok");
-        }
-        else
-        {
-            Console.WriteLine("Error");
-        }
-        var epoch = Apsis.Time.Epoch.JulianDayFromGregorian(y,  m, d, h);
-        Console.WriteLine($"Epoch: {epoch}");
-        
-
-    } else if (input == "n")
-    {
         long year = 2192;
         long month = 2;
         long day = 13;
@@ -69,7 +15,6 @@ while (true)
         Console.WriteLine($"Setting epoch as {year}-{month}-{day} {ut1} Hours");
         Console.WriteLine($"Current epoch in stored form: {epoch.JulianMicroseconds}");
         Console.WriteLine($"Current epoch in display form: {epoch.JulianDay}");
-        
         
         long start = Stopwatch.GetTimestamp(); // Nanoseconds
         
@@ -93,10 +38,6 @@ while (true)
         var tmpJulianMicroseconds = epoch + elapsed;
         Console.WriteLine($"new Julian Microseconds: {tmpJulianMicroseconds}");
         Console.WriteLine($"Converted from the stored + elapsed: {tmpJulianMicroseconds.JulianMicroseconds} microseconds,  {tmpJulianMicroseconds.JulianDay} Julian Days");
-    }217876147203000071 217876147203000071
-    
-    
-    
 }
 
 
---
+++ src/Apsis.Coordinates/Coordinate.cs
@@ -0,0 +1,15 @@
+﻿namespace Apsis.Coordinates;
+
+/// <summary>
+/// ECEF is the primary value
+/// Geodetic Longitude Latitude Altitude are derived from ECEF
+/// GeodeticLatitude
+/// Longitude
+/// GeodeticAltitude
+/// </summary>
+public readonly record struct Coordinate(long ecef_x, long ecef_y, long ecef_z);
+{
+    
+    
+    
+}
